b660b7e3229b2681860d96031571cf4a05c2a102
Paweł hamryszak <phamryszak91@gmail.com>
2023-06-12 13:52:04 +0200

    first commit

--- src/SetupForm.js
+++ src/SetupForm.js
@@ -1,73 +1,71 @@
-import React from "react";
-import { useGlobalContext } from "./context";
+import React from 'react'
+import { useGlobalContext } from './context'
 
 const SetupForm = () => {
-  const { quiz, handleChange, handleSubmit, error } = useGlobalContext();
+  const { quiz, handleChange, handleSubmit, error } = useGlobalContext()
   return (
     <main>
-      <div className="section quiz quiz-small">
-        <form className="setup-form">
+      <section className='quiz quiz-small'>
+        <form className='setup-form'>
           <h2>setup quiz</h2>
-
           {/* amount */}
-          <div className="form-control">
-            <label htmlFor="amount">number of question</label>
+          <div className='form-control'>
+            <label htmlFor='amount'>number of questions</label>
             <input
-              type="number"
-              name="amount"
-              id="amount"
+              type='number'
+              name='amount'
+              id='amount'
               value={quiz.amount}
               onChange={handleChange}
-              className="form-input"
+              className='form-input'
               min={1}
               max={50}
             />
-
-            {/* category */}
-            <div className="form-control">
-              <label htmlFor="category">category</label>
-              <select
-                name="category"
-                id="category"
-                className="form-input"
-                value={quiz.category}
-                onChange={handleChange}
-              >
-                <option value="history">history</option>
-                <option value="sports">sports</option>
-                <option value="politics">politics</option>
-              </select>
-            </div>
           </div>
+          {/* category */}
 
-          {/* difficulty */}
-          <div className="form-control">
-            <label htmlFor="difficulty"> select difficulty</label>
+          <div className='form-control'>
+            <label htmlFor='category'>category</label>
             <select
-              name="difficulty"
-              id="difficulty"
-              className="form-input"
+              name='category'
+              id='category'
+              className='form-input'
+              value={quiz.category}
+              onChange={handleChange}
+            >
+              <option value='sports'>sports</option>
+              <option value='history'>history</option>
+              <option value='politics'>politics</option>
+            </select>
+          </div>
+          {/* difficulty */}
+
+          <div className='form-control'>
+            <label htmlFor='difficulty'>select difficulty</label>
+            <select
+              name='difficulty'
+              id='difficulty'
+              className='form-input'
               value={quiz.difficulty}
               onChange={handleChange}
             >
-              <option value="easy">easy</option>
-              <option value="medium">medium</option>
-              <option value="hard">hard</option>
+              <option value='easy'>easy</option>
+              <option value='medium'>medium</option>
+              <option value='hard'>hard</option>
             </select>
           </div>
-
           {error && (
-            <p className="error">
+            <p className='error'>
               can't generate questions, please try different options
             </p>
           )}
-          <button type="submit" onClick={handleSubmit} className="submit-btn">
+          <button type='submit' onClick={handleSubmit} className='submit-btn'>
             start
           </button>
         </form>
-      </div>
+      </section>
     </main>
-  );
-};
+  )
+}
 
-export default SetupForm;
+export default SetupForm
--- src/context.js
+++ src/context.js
@@ -1,104 +1,91 @@
-import axios from "axios";
-import React, { useState, useContext, useEffect } from "react";
+import axios from 'axios'
+import React, { useState, useContext, useEffect } from 'react'
 
 const table = {
   sports: 21,
   history: 23,
   politics: 24,
-};
+}
 
-const API_ENDPOINT = "https://opentdb.com/api.php?";
+const API_ENDPOINT = 'https://opentdb.com/api.php?'
 
-const url = "";
-
+const url = ''
 const tempUrl =
-  "https://opentdb.com/api.php?amount=10&category=23&difficulty=easy&type=multiple";
-
-const AppContext = React.createContext();
+  'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple'
+const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  const [waiting, setWaiting] = useState(true);
-
-  const [loading, setLoading] = useState(false);
-
-  const [questions, setQuestions] = useState([]);
-
-  const [index, setIndex] = useState(0);
-
-  const [correct, setCorrect] = useState(0);
-
-  const [error, setError] = useState(false);
-
+  const [waiting, setWaiting] = useState(true)
+  const [loading, setLoading] = useState(false)
+  const [questions, setQuestions] = useState([])
+  const [index, setIndex] = useState(0)
+  const [correct, setCorrect] = useState(0)
+  const [error, setError] = useState(false)
   const [quiz, setQuiz] = useState({
     amount: 10,
-    category: "history",
-    difficulty: "easy",
-  });
-
-  const [isModalOpen, setIsModalOpen] = useState(false);
+    category: 'sports',
+    difficulty: 'easy',
+  })
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   const fetchQuestions = async (url) => {
-    setLoading(true);
-    setWaiting(false);
-    const response = await axios(url).catch((err) => console.log(err));
+    setLoading(true)
+    setWaiting(false)
+    const response = await axios(url).catch((err) => console.log(err))
     if (response) {
-      const data = response.data.results;
+      const data = response.data.results
       if (data.length > 0) {
-        setQuestions(data);
-        setLoading(false);
-        setWaiting(false);
-        setError(false);
+        setQuestions(data)
+        setLoading(false)
+        setWaiting(false)
+        setError(false)
       } else {
-        setWaiting(true);
-        setError(true);
+        setWaiting(true)
+        setError(true)
       }
     } else {
-      setWaiting(true);
+      setWaiting(true)
     }
-  };
+  }
 
   const nextQuestion = () => {
     setIndex((oldIndex) => {
-      const index = oldIndex + 1;
+      const index = oldIndex + 1
       if (index > questions.length - 1) {
-        openModal();
-        return 0;
+        openModal()
+        return 0
       } else {
-        return index;
+        return index
       }
-    });
-  };
-
+    })
+  }
   const checkAnswer = (value) => {
     if (value) {
-      setCorrect((oldState) => oldState + 1);
+      setCorrect((oldState) => oldState + 1)
     }
-    nextQuestion();
-  };
+    nextQuestion()
+  }
 
   const openModal = () => {
-    setIsModalOpen(true);
-  };
-
+    setIsModalOpen(true)
+  }
   const closeModal = () => {
-    setWaiting(true);
-    setCorrect(0);
-    setIsModalOpen(false);
-  };
-
+    setWaiting(true)
+    setCorrect(0)
+    setIsModalOpen(false)
+  }
   const handleChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    setQuiz({ ...quiz, [name]: value });
-  };
-
+    const name = e.target.name
+    const value = e.target.value
+    setQuiz({ ...quiz, [name]: value })
+  }
   const handleSubmit = (e) => {
-    e.preventDefault();
-    const { amount, category, difficulty } = quiz;
+    e.preventDefault()
+    const { amount, category, difficulty } = quiz
 
-    const url = `${API_ENDPOINT}amount=${amount}=&difficulty=${difficulty}&category=${table[category]}&type=multiple`;
-    fetchQuestions(url);
-  };
+    const url = `${API_ENDPOINT}amount=${amount}&difficulty=${difficulty}&category=${table[category]}&type=multiple`
+    fetchQuestions(url)
+  }
 
   return (
     <AppContext.Provider
@@ -120,11 +107,11 @@ const AppProvider = ({ children }) => {
     >
       {children}
     </AppContext.Provider>
-  );
-};
+  )
+}
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext);
-};
+  return useContext(AppContext)
+}
 
-export { AppContext, AppProvider };
+export { AppContext, AppProvider }
